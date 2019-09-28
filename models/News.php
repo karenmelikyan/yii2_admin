db73f62ec6c8d16acccc2e8ff6b3c3fa79e94a94
Karen Melikyan <karenmelikyan2018@gmail.com>
2019-09-28 17:53:25 +0400

    begin project

---
+++ models/News.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+class News extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'news';
+    }
+}
