bfd2e14404cc7f37259f58de66dc352a85ba6b9d
Karen Melikyan <karenmelikyan2018@gmail.com>
2019-10-06 17:42:34 +0400

    optimizated code

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -12,6 +12,7 @@ use yii\web\NotFoundHttpException;
 class NewsController extends Controller
 {
     /**
+     * Show all articles
      *
      */
      public function actionIndex()
@@ -26,7 +27,7 @@ class NewsController extends Controller
      }
 
     /**
-     * Show all articles
+     * Show one article
      *
      * @param $id
      * @return string
@@ -35,29 +36,44 @@ class NewsController extends Controller
     public function actionShow($id)
     {
         $article = Article::findOne(['id' => $id]);
+        $categories = Category::find()->all();
 
         if ($article === null) {
             throw new NotFoundHttpException('Article is not found.');
         }
 
         return $this->render('show', [
-            'article' => $article
+            'article' => $article,
+            'categories' => $categories
         ]);
     }
 
     /**
+     * Show article by chosen category
      * @param $id
      * @return string
      */
     public function actionCategory($id)
     {
-        $articles = Article::findAll(['news_category' => $id]);
-        $categories = Category::find()->all();
+        //if no `all` category is selected
+        if ($id > 1) {
+            $articles = Article::findAll(['news_category' => $id, 'status' => Article::STATUS_PUBLISHED]);
+            $categories = Category::find()->all();
 
-        return $this->render('index', [
-            'articles' => $articles,
-            'categories' => $categories
-        ]);
+            //if url not content right id for particular category
+            if (count($categories) < $id) {
+                throw new NotFoundHttpException('There is no such category');
+            }
+
+            return $this->render('index', [
+                'articles' => $articles,
+                'categories' => $categories
+            ]);
+
+        }else{
+            return $this->redirect(['news/']);
+        }
+
 
 //        $rows = (new \yii\db\Query())
 //            ->select(['id', 'email'])
--- views/news/show.php
+++ views/news/show.php
@@ -2,7 +2,18 @@
 /**
  * @var \app\models\Article $article
  */
+
+use yii\helpers\StringHelper;
+use yii\helpers\Url;
+
 ?>
+
+<?php foreach($categories as $category): ?>
+    <div class="child">
+        <h3><a href="<?= Url::to(['news/category', 'id' => $category->id]); ?>"><?=$category->name?></a></h3>
+    </div>
+<?php endforeach; ?>
+
 <div>
     <h2><?= $article->title; ?></h2>
     <p><?= $article->content; ?></p>
